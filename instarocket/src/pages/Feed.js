964ead4bc28f4526a9aa99a3a52cc3a10d7c00f5
Fabiano Brancher <fabianobrancher@gmail.com>
2019-06-15 00:52:16 -0300

    react-native instarocket

---
+++ instarocket/src/pages/Feed.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import api from '../services/api'
+
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+
+import camera from '../assets/camera.png'
+import { FlatList } from 'react-native-gesture-handler';
+
+export default class Feed extends Component {
+    static navigationOptions = ({ navigation }) => ({
+        headerRight: (
+            <TouchableOpacity style={{ marginRight: 20 }} onPress={() => navigation.navigate('New')} >
+                <Image source={camera} />
+            </TouchableOpacity>
+        )
+    });
+
+    state = {
+        feed: []
+    }
+
+    async componentDidMount() {
+        // this.registerToSocket()
+
+        const response = await api.get('posts')
+
+        this.setState({ feed: response.data })
+    }
+
+    render() {
+        return (
+            <View>
+                <FlatList data={this.state.feed}
+                    keyExtractor={post => post._id}
+                    renderItem={({ item }) => (
+                        <Text>{item.author}</Text>
+                    )}
+                />
+
+
+            </View>
+        )
+    }
+}
